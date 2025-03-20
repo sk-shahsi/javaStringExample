d8b9bb55d972e665edd1b484979049bc76dc3195
ErAashishRanjan <er.aashishranjan@gmail.com>
2025-03-20 16:58:47 +0530

    first

---
+++ Lab464.java
@@ -0,0 +1,8 @@
+public class Lab464 {
+    public static void main(String[] args) {
+        String s=null;
+        String ss=String.valueOf(s);
+        System.out.println(ss);
+
+    }
+}
---
+++ Lab465.java
@@ -0,0 +1,11 @@
+public class Lab465 {
+
+
+    public static void main(String[] args) {
+        String s="ram";
+        int len=s.length();
+        System.out.println(len);
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+    }
+}
